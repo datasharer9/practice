8e9356268533867e9e19cb6e1f1b4aaf651a0439
Ku HeeSeong <38518464+datasharer9@users.noreply.github.com>
2022-07-24 18:07:11 +0900

    인터넷프로그래밍 수업 예제

---
+++ webPrj/src/webPrj/javaFile.java
@@ -0,0 +1,42 @@
+package webPrj;
+
+import com.sun.java.util.jar.pack.Instruction.Switch;
+
+public class javaFile {
+	public static void main(String[] args) {
+		System.out.println();
+		System.out.println();
+		
+		int numbering1;
+		int numbering2;
+		int numbering3;
+		int numbering4;
+		
+		
+		for (int i = 0; i < args.length; i++) {
+			
+		}
+		
+		if (condition) {
+			
+		}
+		System.out.println();
+		switch (key) {
+		case value:
+			
+			break;
+
+		default:
+			break;
+		}
+		
+		if (condition) {
+			
+		} else {
+
+		}
+		
+		
+	}
+	
+}
